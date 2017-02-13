f09da6a3313cdcde01b89f96e30f52750da9ade4
Matheus Kautzmann <kautzmann5@gmail.com>
2017-02-13 17:24:36 -0200

    z-checkbox source

---
+++ src/component.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2017 z-kit contributors.
+ * @license MIT LICENSE
+ * https://github.com/z-kit/z-checkbox
+ */
+
+import styles from './style.css';
+
+// Global to local style mappings, works even if using CSS modules
+const classnames = {
+  wrapper: styles['z-checkbox'] || 'z-checkbox',
+  box: styles['z-checkbox__box'] || 'z-checkbox__box',
+  input: styles['z-checkbox__input'] || 'z-checkbox__input',
+  container: styles['z-checkbox__container'] || 'z-checkbox__container',
+  label: styles['z-checkbox__label'] || 'z-checkbox__label',
+  labelTop: styles['z-checkbox__label--top'] || 'z-checkbox__label--top',
+  labelBottom: styles['z-checkbox__label--bottom'] || 'z-checkbox__label--bottom',
+  labelLeft: styles['z-checkbox__label--left'] || 'z-checkbox__label--left',
+  isSuccess: styles['z-checkbox__container--success'] || 'z-checkbox__container--success',
+  isWarning: styles['z-checkbox__container--warning'] || 'z-checkbox__container--warning',
+  isDanger: styles['z-checkbox__container--danger'] || 'z-checkbox__container--danger',
+  isError: styles['z-checkbox__container--error'] || 'z-checkbox__container--error',
+};
+
+export function ZCheckbox(e) {
+  return (props) => {
+    const { label, labelPosition, success, warning, danger, error } = props;
+    const labelClasses = [classnames.label];
+    if (labelPosition === 'top') labelClasses.push(classnames.labelTop);
+    if (labelPosition === 'bottom') labelClasses.push(classnames.labelBottom);
+    if (labelPosition === 'left') labelClasses.push(classnames.labelLeft);
+    const labelProps = { className: labelClasses.join(' ') };
+    if (props.id) labelProps.for = props.id;
+    const eLabel = label ? e('label', labelProps, label) : null;
+    const eInput = e('input', { className: classnames.input, type: 'checkbox', props });
+    const containerClasses = [classnames.container];
+    if (success) containerClasses.push(classnames.isSuccess);
+    if (warning) containerClasses.push(classnames.isWarning);
+    if (danger) containerClasses.push(classnames.isDanger);
+    if (error) containerClasses.push(classnames.isError);
+    const eContainer = e('div', { className: containerClasses.join(' ') });
+    const eBox = e('div', { className: classnames.box }, [eInput, eContainer]);
+    const checkbox = (labelPosition === 'top' || labelPosition === 'left')
+      ? [eLabel, eBox]
+      : [eBox, eLabel];
+    return e('div', { className: classnames.wrapper }, checkbox);
+  };
+}
+
+export default ZCheckbox;
---
+++ stories/index.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import checkbox from '../src/component';
+
+const ZCheckbox = checkbox(React.createElement);
+
+storiesOf('CSS component', module)
+  .add('default', () => (
+    <div className="z-checkbox">
+      <div className="z-checkbox__box">
+        <input className="z-checkbox__input" type="checkbox" />
+        <div className="z-checkbox__container" />
+      </div>
+    </div>
+  ))
+  .add('colors', () => (
+    <div>
+      <div className="z-checkbox">
+        <div className="z-checkbox__box">
+          <input id="test" className="z-checkbox__input" type="checkbox" />
+          <div className="z-checkbox__container z-checkbox__container--success" />
+        </div>
+      </div>
+      <div className="z-checkbox">
+        <div className="z-checkbox__box">
+          <input id="test" className="z-checkbox__input" type="checkbox" />
+          <div className="z-checkbox__container z-checkbox__container--warning" />
+        </div>
+      </div>
+      <div className="z-checkbox">
+        <div className="z-checkbox__box">
+          <input id="test" className="z-checkbox__input" type="checkbox" />
+          <div className="z-checkbox__container z-checkbox__container--danger" />
+        </div>
+      </div>
+      <div className="z-checkbox">
+        <div className="z-checkbox__box">
+          <input id="test" className="z-checkbox__input" type="checkbox" />
+          <div className="z-checkbox__container z-checkbox__container--error" />
+        </div>
+      </div>
+    </div>
+  ))
+  .add('label', () => (
+    <div>
+      <div className="z-checkbox">
+        <div className="z-checkbox__box">
+          <input id="test" className="z-checkbox__input" type="checkbox" />
+          <div className="z-checkbox__container" />
+        </div>
+        <label htmlFor="test" className="z-checkbox__label">Label</label>
+      </div>
+      <div className="z-checkbox">
+        <label htmlFor="test2" className="z-checkbox__label z-checkbox__label--left">Label</label>
+        <div className="z-checkbox__box">
+          <input id="test2" className="z-checkbox__input" type="checkbox" />
+          <div className="z-checkbox__container" />
+        </div>
+      </div>
+      <div className="z-checkbox">
+        <label htmlFor="test3" className="z-checkbox__label z-checkbox__label--top">Label</label>
+        <div className="z-checkbox__box">
+          <input id="test3" className="z-checkbox__input" type="checkbox" />
+          <div className="z-checkbox__container" />
+        </div>
+      </div>
+      <div className="z-checkbox">
+        <div className="z-checkbox__box">
+          <input id="test4" className="z-checkbox__input" type="checkbox" />
+          <div className="z-checkbox__container" />
+        </div>
+        <label htmlFor="test4" className="z-checkbox__label z-checkbox__label--bottom">Label</label>
+      </div>
+    </div>
+  ));
+
+storiesOf('Stateless functional component', module)
+  .add('default', () => <ZCheckbox />)
+  .add('colors', () => (
+    <div>
+      <ZCheckbox success />
+      <ZCheckbox warning />
+      <ZCheckbox danger />
+      <ZCheckbox error />
+    </div>
+  ))
+  .add('label', () => (
+    <div>
+      <ZCheckbox label="Label" />
+      <ZCheckbox label="Label" labelPosition="left" />
+      <ZCheckbox label="Label" labelPosition="top" />
+      <ZCheckbox label="Label" labelPosition="bottom" />
+    </div>
+  ));

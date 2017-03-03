cb278262d2b6c8f2fe91c4eeebbc4bd5f6fd4bba
Matheus Kautzmann <kautzmann5@gmail.com>
2017-03-03 11:48:39 -0300

    Migrate from nightwatch -> nightmare

--- test/regression/suite/component.js
+++ test/regression/suite/component.js
@@ -1,43 +1,101 @@
-module.exports = {
-  default: (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=default')
-      .waitForElementPresent('.z-checkbox', 1000)
-      .assert.attributeEquals('.z-checkbox__input', 'type', 'checkbox')
-      .end();
-  },
-  colors: (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=colors')
-      .waitForElementPresent('.z-checkbox', 1000)
-      .getCssProperty('.z-checkbox__container--success', 'background-color', (result) => {
-        browser.assert.equal(result.value, 'rgba(118, 178, 69, 1)');
-      })
-      .getCssProperty('.z-checkbox__container--warning', 'background-color', (result) => {
-        browser.assert.equal(result.value, 'rgba(254, 215, 102, 1)');
-      })
-      .getCssProperty('.z-checkbox__container--danger', 'background-color', (result) => {
-        browser.assert.equal(result.value, 'rgba(240, 58, 71, 1)');
-      })
-      .getCssProperty('.z-checkbox__container--error', 'background-color', (result) => {
-        browser.assert.equal(result.value, 'rgba(255, 0, 0, 1)');
-      })
-      .end();
-  },
-  label: (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=label')
-      .waitForElementPresent('.z-checkbox', 1000)
-      .assert.containsText('.z-checkbox__label:nth-of-type(1)', 'Label')
-      .end();
-  },
-  'disabled input': (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=disabled%20input')
-      .waitForElementPresent('.z-checkbox', 1000)
-      .getCssProperty('.z-checkbox__container', 'opacity', (result) => {
-        browser.assert.equal(result.value, '0,5');
-      })
-      .end();
-  },
-};
+import { test } from 'ava';
+import Nightmare from 'nightmare';
+
+test('SFC component - default', (t) => {
+  t.plan(1);
+  const msg = 'should render an input type checkbox';
+  const expected = 'checkbox';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=default')
+    .wait('.z-checkbox')
+    .evaluate(() => document.querySelector('.z-checkbox__input').getAttribute('type'))
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - colors', (t) => {
+  t.plan(1);
+  const msg = 'should render color for success modifier';
+  const expected = 'rgb(118, 178, 69)';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=colors')
+    .wait('.z-checkbox')
+    .evaluate(() => {
+      const checkboxStyle = getComputedStyle(document.querySelector('.z-checkbox__container--success'));
+      return checkboxStyle.backgroundColor;
+    })
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - colors', (t) => {
+  t.plan(1);
+  const msg = 'should render color for warning modifier';
+  const expected = 'rgb(254, 215, 102)';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=colors')
+    .wait('.z-checkbox')
+    .evaluate(() => {
+      const checkboxStyle = getComputedStyle(document.querySelector('.z-checkbox__container--warning'));
+      return checkboxStyle.backgroundColor;
+    })
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - colors', (t) => {
+  t.plan(1);
+  const msg = 'should render color for danger modifier';
+  const expected = 'rgb(240, 58, 71)';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=colors')
+    .wait('.z-checkbox')
+    .evaluate(() => {
+      const checkboxStyle = getComputedStyle(document.querySelector('.z-checkbox__container--danger'));
+      return checkboxStyle.backgroundColor;
+    })
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - colors', (t) => {
+  t.plan(1);
+  const msg = 'should render color for error modifier';
+  const expected = 'rgb(255, 0, 0)';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=colors')
+    .wait('.z-checkbox')
+    .evaluate(() => {
+      const checkboxStyle = getComputedStyle(document.querySelector('.z-checkbox__container--error'));
+      return checkboxStyle.backgroundColor;
+    })
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - label', (t) => {
+  t.plan(1);
+  const msg = 'should render the label';
+  const expected = 'Label';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=label')
+    .wait('.z-checkbox')
+    .evaluate(() => document.querySelector('.z-checkbox__label:nth-of-type(1)').textContent)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('SFC component - disabled input', (t) => {
+  t.plan(1);
+  const msg = 'should render the disabled checkbox style';
+  const expected = 0;
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=disabled%20input')
+    .wait('.z-checkbox')
+    .evaluate(() => {
+      const checkboxStyle = getComputedStyle(document.querySelector('.z-checkbox__container'));
+      return parseInt(checkboxStyle.opacity, 10);
+    })
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
